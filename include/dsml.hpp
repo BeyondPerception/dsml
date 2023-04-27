e8896a309fb24e42c915bdb43ae1be0bce75f95a
Ronak Malik <ronakmalik4242@gmail.com>
2023-04-27 16:50:53 -0400

    Change dsml header

--- include/dsml.hpp
+++ include/dsml.hpp
@@ -8,20 +8,22 @@ class DSML
 public:
     DSML(std::string config);
 
-    int register_owner(std::string program_name, std::string program_ip);
+    int register_owner(std::string variable_owner, std::string location);
+
+    int register_owner(std::string variable_owner, int socket);
 
     template <typename T>
     T get(std::string var);
 
     template <typename T>
+    void get(std::string var, T& valueRet);
+
+    template <typename T>
     void set(std::string var, T value);
 
 private:
-    std::unordered_map<std::string, void *> vars;
+    std::unordered_map<std::string, void*> vars;
 
     template <typename T>
     void create(std::string var);
-
-    template <typename T>
-    void create(std::string var, T value);
 };
