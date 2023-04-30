7753e151bb28a190d7ac4750f5f34d664a166167
Ronak Malik <ronakmalik4242@gmail.com>
2023-04-30 19:09:44 -0400

    Update demo code

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,13 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-    dsml::State dsml ("./demo/config.tsv");
+    dsml::State dsml ("../demo/config.tsv", "CN");
 
-    dsml.set("TEST1", 5);
-    dsml.set("TEST2", 255);
+    dsml.set("TEST1", (int8_t)-128);
+    dsml.set("TEST2", (uint8_t)255);
 
     int test1 = dsml.get<int8_t>("TEST1");
-    int test2 = dsml.get<int8_t>("TEST1");
+    int test2 = dsml.get<uint8_t>("TEST2");
 
     std::cout << test1 << " " << test2 << "\n";
 
--- demo/demo2.cpp
+++ demo/demo2.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    dsml::State dsml ("./demo/config.tsv");
+    dsml::State dsml ("./demo/config.tsv", "DN");
     
     dsml.register_owner("CN", "127.0.0.1", 1111);
 
