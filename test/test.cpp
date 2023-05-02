5b74110d945fdf28afe617efe95fafbc5f314a58
Albert Qi <albertqi03@gmail.com>
2023-05-02 17:07:34 -0400

    Test documentation

--- test/test.cpp
+++ test/test.cpp
@@ -8,6 +8,12 @@
 
 bool all_tests_passed = true;
 
+/**
+ * Test a boolean expression and print the result.
+ *
+ * @param b Input boolean.
+ * @param msg Message to print.
+ */
 void test(const bool &b, std::string msg)
 {
     const int i = msg.size();
@@ -17,8 +23,14 @@ void test(const bool &b, std::string msg)
     std::cerr << msg << std::endl;
 }
 
+/**
+ * Run simple tests.
+ *
+ * @param dsml Instance of `dsml::State`.
+ */
 void test_simple(dsml::State &dsml)
 {
+    // Set variables.
     dsml.set("TEST1", (int8_t)-1);
     dsml.set("TEST2", (int16_t)-2);
     dsml.set("TEST3", (int32_t)-3);
@@ -179,20 +191,61 @@ void test_simple(dsml::State &dsml)
     }
 }
 
-void test_hard(dsml::State &dmsl1, dsml::State &dmsl2)
+/**
+ * Run hard tests.
+ *
+ * @param dsml1 First instance of `dsml::State`.
+ * @param dsml2 Second instance of `dsml::State`.
+ */
+void test_hard(dsml::State &dsml1, dsml::State &dsml2)
 {
+    // Test `get`.
+    test(dsml2.get<int8_t>("TEST1") == -1, "set/get INT8");
+    test(dsml2.get<int16_t>("TEST2") == -2, "set/get INT16");
+    test(dsml2.get<int32_t>("TEST3") == -3, "set/get INT32");
+    test(dsml2.get<int64_t>("TEST4") == -4, "set/get INT64");
+    test(dsml2.get<uint8_t>("TEST5") == 5, "set/get UINT8");
+    test(dsml2.get<uint16_t>("TEST6") == 6, "set/get UINT16");
+    test(dsml2.get<uint32_t>("TEST7") == 7, "set/get UINT32");
+    test(dsml2.get<uint64_t>("TEST8") == 8, "set/get UINT64");
+    test(dsml2.get<float>("TEST9") == -9.0, "set/get FLOAT");
+    test(dsml2.get<double>("TEST10") == -10.0, "set/get DOUBLE");
+    test(dsml2.get<std::vector<int8_t>>("TEST11") == std::vector<int8_t>{-1, 0, 1}, "set/get ARRAY");
+    test(dsml2.get<std::string>("TEST12") == "Hello world!", "set/get STRING");
+
+    // Set variables.
+    dsml1.set("TEST1", (int8_t)42);
+    std::vector<int8_t> v = dsml1.get<std::vector<int8_t>>("TEST11");
+    v.push_back(42);
+    dsml1.set("TEST11", v);
+    dsml1.set("TEST12", std::string("Goodbye world!"));
+
+    // Test `get` again.
+    test(dsml2.get<int8_t>("TEST1") == 42, "set/get INT8 again");
+    test(dsml2.get<std::vector<int8_t>>("TEST11") == std::vector<int8_t>{-1, 0, 1, 42}, "set/get ARRAY again");
+    test(dsml2.get<std::string>("TEST12") == "Goodbye world!", "set/get STRING again");
+
+    // Set variables.
+    dsml2.set("TEST1", (int8_t)24);
+    dsml2.set("TEST11", std::vector<int8_t>{-24, 24});
+    dsml2.set("TEST12", std::string("..."));
+
+    // Test `get` after requesting updates.
+    test(dsml1.get<int8_t>("TEST1") == 24, "set/get INT8 request update");
+    test(dsml1.get<std::vector<int8_t>>("TEST11") == std::vector<int8_t>{-24, 24}, "set/get ARRAY request update");
+    test(dsml1.get<std::string>("TEST12") == "...", "set/get STRING request update");
 }
 
 int main()
 {
-    // Create first `dsml::State` instance.
+    // Create first instance of `dsml::State`.
     dsml::State dsml1("../test/config.tsv", "DSML1", 1111);
 
     // Run simple tests.
     std::cerr << "\nRUNNING SIMPLE TESTS..." << std::endl;
     test_simple(dsml1);
 
-    // Create second `dsml::State` instance.
+    // Create second instance of `dsml::State`.
     dsml::State dsml2("../test/config.tsv", "DSML2", 1112);
     dsml2.register_owner("DSML1", "127.0.0.1", 1111);
 
@@ -201,6 +254,6 @@ int main()
     test_hard(dsml1, dsml2);
 
     // Print results.
-    std::string msg = all_tests_passed ? "\nALL TESTS PASSED :)\n" : "\nSOME TESTS FAILED :(\n";
+    const std::string msg = all_tests_passed ? "\nALL TESTS PASSED :)\n" : "\nSOME TESTS FAILED :(\n";
     std::cerr << msg << std::endl;
 }
